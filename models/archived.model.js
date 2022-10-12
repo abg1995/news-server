ebca4d1f6d9c456e7855d564c72f5599633b41f6
alejandro <aleblancogovantes@hotmail.com>
2022-10-12 11:27:19 +0200

    creado archived model

---
+++ models/archived.model.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const archivedSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+
+module.exports = model("Archived", archivedSchema);
+
