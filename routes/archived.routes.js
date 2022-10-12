04373f75c851793d13794d34170d2a10d13c7801
alejandro <aleblancogovantes@hotmail.com>
2022-10-12 10:49:45 +0200

    correccion de errores en routes, de archived a news, news model date modified a Date data type

--- routes/archived.routes.js
+++ routes/archived.routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const { default: mongoose } = require('mongoose');
-const News = require('../models/news.model');
+const { default: mongoose } = require("mongoose");
+const News = require("../models/news.model");
 
-
-
+//GET list of news
 
 
 
--- routes/news.routes.js
+++ routes/news.routes.js
@@ -2,7 +2,17 @@ const router = require("express").Router();
 const { default: mongoose } = require('mongoose');
 const News = require('../models/news.model');
 
-
+router.get("/news", (req, res, next) => {
+    News.find()
+      .then((response) => res.json(response))
+      .catch((err) => {
+        console.log("error displaying the news", err);
+        res.status(500).json({
+          message: "error displaying news",
+          error: err,
+        });
+      });
+  });
 
 
 
