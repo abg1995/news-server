adebd7e06affb1d8caeef1b446bf8cce578f3553
alejandro <aleblancogovantes@hotmail.com>
2022-10-13 10:01:46 +0200

    com,ment added

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const { default: mongoose } = require('mongoose');
 const News = require('../models/news.model');
 
+//GET list of news
+
 router.get("/news", (req, res, next) => {
     News.find()
       .then((response) => res.json(response))
