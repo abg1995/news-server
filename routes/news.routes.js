6bbd94d57f38663f6642607786f07768f19af89e
alejandro <aleblancogovantes@hotmail.com>
2022-10-14 21:04:31 +0200

    route news to move news from news to archived

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { default: mongoose } = require('mongoose');
 const News = require('../models/news.model');
+const Archived = require('../models/archived.model');
 
 //GET list of news
 
-router.get("/", (req, res, next) => {
+router.get("/news", (req, res, next) => {
     News.find()
       .then((response) => res.json(response))
       .catch((err) => {
@@ -17,6 +18,38 @@ router.get("/", (req, res, next) => {
   });
 
 
+//PUT ROUTE TO MOVE NEWS TO ARCHIVED
 
+router.put("/news/:newsId", (req,res,next) => {
+    const {newsId} = req.params;
+    let newsForArchive;
+    News.findById(newsId)
+    .then(response=>{
+        newsForArchive= {
+            title : response.title,
+            text : response.text,
+            image: response.image || null,
+            date:  response.date || null
+        }
+        return News.findByIdAndDelete(newsId)
+    })
+        .then( () =>{
+            console.log("RESPONSEEEEE    "+ newsForArchive)
+            return Archived.create(newsForArchive)
+        })
+        .then( (response) => {
+            console.log("LOOK AT THIS REPSONSE" + response)
+            res.status(201).json(response)
+        })
+        .catch((err) => {
+            console.log("error updating the news posistion", err)
+            res.status(500).json({
+                message: "error changiong the news to archived",
+                error: err
+            });
+        });
+})
+
+ 
 
 module.exports = router;
